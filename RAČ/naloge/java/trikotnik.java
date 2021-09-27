e6db8a42b4ab4cbc8a06fa6a62efefd549d1071f
Anže Pintar <anze.pintar2@gmail.com>
2021-09-27 17:26:13 +0200

    Update trikotnik.java

--- RAČ/naloge/java/trikotnik.java
+++ RAČ/naloge/java/trikotnik.java
@@ -2,23 +2,21 @@ import java.util.Scanner;
 
 class trikotnik {
   public static void main(String[] args) {
-    float a = 10.0f;
-    float b = 10.0f;
     Scanner vnos = new Scanner(System.in);
-    System.out.println("Vnesi A"); 
-    Float A = vnos.nextFloat();
-    System.out.println("Vnesi B"); 
-    Float B = vnos.nextFloat();
-    Double C = Math.sqrt(A*A+B*B);
-    Double alfa = Math.acos((C*C + A*A - B*B)/(2*C*A));
+    System.out.println("Vnesi a"); 
+    Float a = vnos.nextFloat();
+    System.out.println("Vnesi b"); 
+    Float b = vnos.nextFloat();
+    Double c = Math.sqrt(a*a+b*b);
+    Double alfa = Math.acos((c*c + a*a - b*b)/(2*c*a));
     alfa = alfa*57.2957795;
-    Double beta = Math.acos((C*C + B*B - A*A)/(2*C*B));
+    Double beta = Math.acos((c*c + b*b - a*a)/(2*c*b));
     beta = beta*57.2957795;
-	Double square = A*B/2.0;
+    Double square = a*b/2.0;
     System.out.println(
-    "A =" + A +
-    " \nB =" + B +
-    " \nC =" + C +
+    "a =" + a +
+    " \nb =" + b +
+    " \nc =" + c +
     " \nalfa =" + alfa +
     " \nbeta =" + beta +
     " \nploščina =" + square 
