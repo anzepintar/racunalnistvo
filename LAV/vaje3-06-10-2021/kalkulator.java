95afc083f9221594054b91a736b70316456e19cb
Anže Pintar <anze.pintar2@gmail.com>
2021-10-13 11:16:56 +0200

    Update kalkulator.java

--- LAV/vaje3-06-10-2021/kalkulator.java
+++ LAV/vaje3-06-10-2021/kalkulator.java
@@ -1,8 +1,49 @@
 import java.util.Scanner;
-public class kalkuator {
+
+public class Kalkulator {
 
 	public static void main(String[] args) {
-		Scanner inpt = new Scanner(System.in);
+
+		char operator;
+		Double num1, num2, rez;
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Izberi operator: +, -, *, or /");
+		operator = input.next().charAt(0);
+
+		System.out.println("Vnesi prvo številko");
+		num1 = input.nextDouble();
+
+		System.out.println("Vnesi drugo številko");
+		num2 = input.nextDouble();
+
+		switch (operator) {
+
+			case '+' :
+				rez = num1 + num2;
+				System.out.println(num1 + " + " + num2 + " = " + rez);
+				break;
+
+			case '-' :
+				rez = num1 - num2;
+				System.out.println(num1 + " - " + num2 + " = " + rez);
+				break;
+
+			case '*' :
+				rez = num1 * num2;
+				System.out.println(num1 + " * " + num2 + " = " + rez);
+				break;
+
+			case '/' :
+				rez = num1 / num2;
+				System.out.println(num1 + " / " + num2 + " = " + rez);
+				break;
+
+			default :
+				System.out.println("Kalkulator podpira samo operatorje +, -, *, /");
+				break;
+		}
 
 	}
 
