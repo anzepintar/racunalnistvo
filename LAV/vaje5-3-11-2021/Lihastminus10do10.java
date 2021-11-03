6aa43892fc1a6365f4956ca55b4320a3c8692817
Anže Pintar <anze.pintar2@gmail.com>
2021-11-03 11:51:54 +0100

    Add files via upload

---
+++ LAV/vaje5-3-11-2021/Lihastminus10do10.java
@@ -0,0 +1,16 @@
+package sola;
+
+public class Lihastminus10do10 {
+
+	public static void main(String[] args) {
+		int i = -10;
+		
+		do{
+			if (i%2!=0)
+				System.out.println(i);
+			i++;
+		}while(i<10);
+
+	}
+
+}
