fb0061652913c8960eb8ff991a8297c6d2a58fdb
Anže Pintar <anze.pintar2@gmail.com>
2021-11-03 12:00:49 +0100

    Add files via upload

---
+++ LAV/vaje5-3-11-2021/Sestevanjestevil1do100.java
@@ -0,0 +1,16 @@
+package sola;
+
+public class Sestevanjestevil1do100 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		int n = 0;
+		while(i<=100){
+			n+=i;
+			i++;
+		}
+		System.out.println(n);
+
+	}
+
+}
