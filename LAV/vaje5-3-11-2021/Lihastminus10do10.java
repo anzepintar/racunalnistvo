4769ffa2edab39ac4d16a9802dd9d3e544461a12
Anže Pintar <anze.pintar2@gmail.com>
2021-11-03 11:53:14 +0100

    Update Lihastminus10do10.java

--- LAV/vaje5-3-11-2021/Lihastminus10do10.java
+++ LAV/vaje5-3-11-2021/Lihastminus10do10.java
@@ -4,12 +4,12 @@ public class Lihastminus10do10 {
 
 	public static void main(String[] args) {
 		int i = -10;
-		
-		do{
-			if (i%2!=0)
+
+		do {
+			if (i % 2 != 0)
 				System.out.println(i);
 			i++;
-		}while(i<10);
+		} while (i < 10);
 
 	}
 
