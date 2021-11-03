d5276f35a720b269bf43f3fca6ed41bd03fe4812
Anže Pintar <anze.pintar2@gmail.com>
2021-11-03 11:47:17 +0100

    Create Deljivos3inne5.java

---
+++ LAV/vaje5-3-11-2021/Deljivos3inne5.java
@@ -0,0 +1,13 @@
+package sola;
+
+public class Deljivos3inne5 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 100; i++){
+			if (i % 3 == 0 && i % 5 != 0)
+				System.out.println(i);
+		}
+
+	}
+
+}
