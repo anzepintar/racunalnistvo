68b03e2fb3b467592b2a78ae3b027a72f35d44a8
Anže Pintar <anze.pintar2@gmail.com>
2021-11-15 12:02:54 +0100

    Create graf2.java

---
+++ RAC/naloge/java/graf2.java
@@ -0,0 +1,18 @@
+class graf2 {
+  public static void main(String[] args) {
+     double y = 0;
+    for (double x = -3; x <= 3; x = x + 0.25) {
+      if (x <= -1)
+        y = x+3;
+      else if (x <= 0)
+        y = x*2+4;
+      else if (x <= 1)
+        y = x*(-2)+4;
+      else
+        y = -x+3;
+
+      System.out.format("%.2f ", x);
+      System.out.format("%.2f\n", y);
+
+  }}
+}
