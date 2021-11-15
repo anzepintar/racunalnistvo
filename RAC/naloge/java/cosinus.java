0d82c3c811b24086ff00ec52f34b21272c313a7b
Dominik Krašovec <54041015+mgreen11m@users.noreply.github.com>
2021-11-15 11:41:50 +0100

    Update cosinus.java

--- RAC/naloge/java/cosinus.java
+++ RAC/naloge/java/cosinus.java
@@ -1,22 +1,20 @@
 package naloge;
-
 public class cosinus {
 
 	public static void main(String[] args) {
-		double kot, m, f, cos1;
-		kot = Math.PI / 3;
-		f = 1;
-		cos1 = 0;
-		for (int n = 0; n <= 5; n++) {
+		double m, f, cos,
+		x = Math.PI / 3,
+		fakulteta = 1;
+		cos = 0;
+		for (int n = 0; n<6; n++) {
 			m = 2 * n;
 			for (int k = 1; k <= m; k++) {
-				f *= k;
+				fakulteta *= k;
 			}
-			double tmp = (Math.pow(-1, n) / f) * Math.pow(kot, m);
-			cos1 += tmp;
+			cos += (Math.pow(-1, n) / fakulteta) * Math.pow(x, m);
 		}
-		System.out.println(cos1);
-		System.out.println(Math.cos(kot));
+		System.out.println(cos);
+
 
 	}
 
