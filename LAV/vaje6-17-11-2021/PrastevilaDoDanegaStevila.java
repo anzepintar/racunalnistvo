590ea79ef77b423e72e322afc745e2c50ec689e8
Anže Pintar <anze.pintar2@gmail.com>
2021-11-17 12:01:45 +0100

    Update PrastevilaDoDanegaStevila.java

--- LAV/vaje6-17-11-2021/PrastevilaDoDanegaStevila.java
+++ LAV/vaje6-17-11-2021/PrastevilaDoDanegaStevila.java
@@ -4,21 +4,21 @@ import java.util.Scanner;
 
 class PrastevilaDoDanegaStevila {
 	public static void main(String args[]) {
-		int n;
+		int stPrastevil;
 		boolean je = true;
 		int stevilo = 3;
 		Scanner vnos = new Scanner(System.in);
 		System.out.println("Vnesi število:");
-		n = vnos.nextInt();
-		if (n >= 1) {
-			System.out.println("Prvih " + n + " praštevil je:");
+		stPrastevil = vnos.nextInt();
+		if (stPrastevil >= 1) {
+			System.out.println("Prvih " + stPrastevil + " praštevil je:");
 			System.out.println(2);
 
 		} else {
 			System.out.println("Vnesi naravno število (število>=1)");
 		}
 
-		for (int i = 2; i <= n;) {
+		for (int i = 2; i <= stPrastevil;) {
 			for (int j = 2; j <= Math.sqrt(stevilo); j++) {
 				if (stevilo % j == 0) {
 					je = false;
