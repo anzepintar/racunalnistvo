0ba1cdd85ef305ea34e477caf07d25509a0a3d48
Anže Pintar <anze.pintar2@gmail.com>
2021-12-01 12:04:05 +0100

    UPT

---
+++ LAV/vaje7-1-12-2021/ProduktNenicelnihStevk.java
@@ -0,0 +1,15 @@
+package sola;
+
+public class ProduktNenicelnihStevk {
+
+	public static void main(String[] args) {
+		int n = 100203;
+		int produkt = 1;
+		while(n>0)
+		{
+			int zadnja = n % 10;
+			
+		}
+	}
+
+}
---
+++ LAV/vaje7-1-12-2021/ZvezdiceKvadrat.java
@@ -0,0 +1,16 @@
+package sola;
+
+public class ZvezdiceKvadrat {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
