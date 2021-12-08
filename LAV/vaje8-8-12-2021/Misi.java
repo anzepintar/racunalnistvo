aa1f8b0c82a48c7d04dfe6c48ef51ec4d4e8deca
Anže Pintar <anze.pintar2@gmail.com>
2021-12-08 12:03:26 +0100

    vaj 8

---
+++ LAV/vaje8-8-12-2021/Misi.java
@@ -0,0 +1,18 @@
+package sola;
+
+public class Misi {
+	public static void main(String[] args) {
+		int misi = 1111111;
+		int stmack;
+		for (stmack = 2; stmack < misi; stmack++) {
+			if (misi % stmack == 0) {
+				System.out.println(
+						"Število miši: " + (misi / stmack) + " Število mačk: " + stmack);
+				break;
+			}
+
+		}
+
+	}
+
+}
