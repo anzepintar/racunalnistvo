199bcc406bc72140746a881336801d16c4aa094b
Anže Pintar <anze.pintar2@gmail.com>
2021-12-19 18:18:34 +0100

    upt

---
+++ src/Racunalnistvo/java/tabele/sortiranjeV1Tabeli.java
@@ -0,0 +1,21 @@
+package Racunalnistvo.java.tabele;
+
+public class sortiranjeV1Tabeli {
+  public static void main(String[] args) {
+    int tab[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int i = 0;
+    int liha1 = 0;
+    while (i < tab.length) {
+      if (tab[i] % 2 == 0) {
+        int t = tab[i];
+        tab[i] = tab[liha1];
+        tab[liha1] = t;
+        liha1++;
+      }
+      i++;
+    }
+    for (int j = 0; j < tab.length; j++)
+      System.out.print(tab[j] + " ");
+
+  }
+}
