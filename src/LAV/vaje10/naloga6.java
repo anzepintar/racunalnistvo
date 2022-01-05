4abaa0491a874b473892b8a45c0a3d4c4eecd0d4
Anže Pintar <anze.pintar2@gmail.com>
2022-01-05 11:21:31 +0100

    upt

--- src/LAV/vaje10/naloga6.java
+++ src/LAV/vaje10/naloga6.java
@@ -1,5 +1,13 @@
 package LAV.vaje10;
 
 public class naloga6 {
-  
+  public static void main(String[] args) {
+    int tab[] = { 1, 2, 3, 4, -5, 6, 7, 0, };
+    int m = tab[0];
+    for (int i = 0; i < tab.length; i++) {
+      if(tab[i]<m)
+        m=tab[i];
+    }
+    System.out.println(m);
+  }
 }
