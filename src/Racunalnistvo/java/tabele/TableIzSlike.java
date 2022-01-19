0cb00c467283e1ddff1a3ae1d9ca3eaec5bd916c
Anže Pintar <anze.pintar2@gmail.com>
2022-01-19 21:13:31 +0100

    idea

---
+++ src/Racunalnistvo/java/tabele/TableIzSlike.java
@@ -0,0 +1,14 @@
+package Racunalnistvo.java.tabele;
+
+public class TableIzSlike {
+  public static void main(String[] args) {
+    int tab1[][] = new int[4][4];
+    int tab2[][] = new int[4][4];
+    int tab3[][] = new int[4][4];
+    int tab4[][] = new int[5][5];
+    int tab5[][] = new int[5][5];
+    int tab6[][] = new int[4][4];
+
+    for(int i = 0; )
+  }
+}
