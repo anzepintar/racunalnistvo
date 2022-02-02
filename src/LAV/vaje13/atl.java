997238f4985b7dcb94d046df390a643c7835da05
Anže Pintar <anze.pintar2@gmail.com>
2022-02-02 11:29:08 +0100

    change row and col atl.java

--- src/LAV/vaje13/atl.java
+++ src/LAV/vaje13/atl.java
@@ -1,38 +1,34 @@
 public class atl {
   public static void main(String[] args) {
     int r[][] = new int[12][6];
-    int stmetov = 0;
+    int vsota = 0;
     double meti = 0;
     filltab(r);
 
     printTab(r);
-    for (int i = 0; i < r.length; i++) {
-      for (int j = 0; j < r[0].length; j++) {
-
-        if (r[i][j] > 0) {
-          stmetov = stmetov + r[i][j];
+    for (int i = 0; i < r[0].length; i++) {
+      for (int j = 0; j < r.length; j++) {
+        if (r[j][i] > 0) {
+          vsota = vsota + r[j][i];
           meti++;
         }
       }
-      povprecjeSerije(i, stmetov, meti);
+      povprecjeSerije(i, vsota, meti);
       meti = 0;
-      stmetov = 0;
-
+      vsota = 0;
     }
-
   }
 
   public static void filltab(int[][] r) {
     for (int i = 0; i < r.length; i++) {
       for (int j = 0; j < r[0].length; j++) {
-        r[i][j] = (int) (Math.random() * 10);
+        r[i][j] = (int) Math.round((Math.random() * 20));
       }
     }
   }
 
   public static void povprecjeSerije(int i, int stmetov, double meti) {
-    System.out.println("Povprečna dolžina metov " + (i + 1) + ". serije je " + String.format("%.2f", stmetov / meti));
-
+    System.out.println("Povprečna dolžina metov " + (i + 1) + ". serije je " + String.format("%.2f", stmetov / meti) + " m");
   }
 
   public static void printTab(int tab[][]) {
