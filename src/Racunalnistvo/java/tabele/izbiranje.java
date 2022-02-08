f12697eedf5e7e93e66282b50d2008c1e2da46c3
Anže Pintar <anze.pintar2@gmail.com>
2022-02-08 19:29:31 +0100

    i tried

--- src/Racunalnistvo/java/tabele/izbiranje.java
+++ src/Racunalnistvo/java/tabele/izbiranje.java
@@ -2,20 +2,21 @@ package Racunalnistvo.java.tabele;
 
 class izbiranje {
   public static void main(String[] args) {
-    int tab[] = { 7, 1, 5, 9, 2, 8, -1, 22, 22, 12, -22 };
-    for (int u = 0; u < tab.length; u++) {
-      int min = tab[u];
-      int index = u;
-      for (int n = u; n < tab.length; n++) {
-        if (tab[n] < min) {
-          min = tab[n];
-          index = n;
-
+    int tab[] = { 7, -1111, 5, 9, 2, 8, -1, 22, 22, 12, -22 };
+    int index = 0;
+    for (int i = 0; i < tab.length; i++) {
+      int min = tab[i];
+      index = i;
+      for (int j = i; j < tab.length; j++) {
+        if (tab[j] < min) {
+          min = tab[j];
+          index = j;
+          
         }
       }
     }
     for (int i = 0; i < tab.length; i++) {
-      System.out.print(tab[i] + " ");
+      System.out.print(tab[i] + " " + index + " ");
     }
   }
 }
