6d30282b8975be4a47072e82a2611236ca55e6eb
Anže Pintar <anze.pintar2@gmail.com>
2022-04-06 21:30:46 +0200

    konstruktorji in metode

--- RAC/razredi/Main.java
+++ RAC/razredi/Main.java
@@ -2,14 +2,15 @@ package razredi;
 
 public class Main {
   public static void main(String[] args) {
-    Ulomek ul1 = new Ulomek(8, 4);
+    Ulomek ul1 = new Ulomek(1000, 2000);
     Ulomek ul2 = new Ulomek(2, 8);
     ul1.izpis();
     ul2.izpis();
     ul1.primerjaj(ul2);
-
+    Ulomek k = new Ulomek(12);
     ul1.sestej(ul2);
     ul1.odstej(ul2);
+    System.out.println("Krajšaj:");
     ul1.krajsaj();
   }
 }
--- RAC/razredi/Ulomek.java
+++ RAC/razredi/Ulomek.java
@@ -12,6 +12,10 @@ public class Ulomek {
     this.im = im;
   }
 
+  public Ulomek(int k) {
+    System.out.println(k + " / " + 1);
+  }
+
   public void izpis() {
     System.out.println(st + "/" + im);
   }
@@ -41,11 +45,21 @@ public class Ulomek {
   }
 
   public void krajsaj() {
-    int st = 0;
-    int im = 0;
 
-    System.out.println(this.st / this.im);
+    int maxDel = NajvecjiDelitelj(this.st, this.im);
+    System.out.println(this.st / maxDel + " / " + this.im / maxDel);
 
+  }
+
+  private int NajvecjiDelitelj(int a, int b) {
+    int maxDel = 1;
+    for (int i = 1; i <= Math.min(a, b); i++) {
+      if (a % i == 0 && b % i == 0) {
+        maxDel = i;
+      }
+    }
+
+    return maxDel;
   }
 
 }
